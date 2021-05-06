a8e24352d9364aece0c2d382ac2e7476f1de85d5
aagondo <aagondo@capitalx.id>
2021-05-06 11:34:34 +0700

    fix lint

--- src/components/atoms/tabs/tabs.tsx
+++ src/components/atoms/tabs/tabs.tsx
@@ -3,7 +3,7 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Theme } from '@material-ui/core/styles/MuiThemeProvider';
-import {INode} from "../../../types/common";
+import { INode } from '../../../types/common';
 
 interface PropTypes {
   options: {
